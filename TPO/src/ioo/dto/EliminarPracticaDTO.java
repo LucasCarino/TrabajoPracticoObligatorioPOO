b91b14b68181952812a46f6bb78c4d8626e80d1b
mafebohorquezh <92961114+mafebohorquezh@users.noreply.github.com>
2023-06-25 13:51:10 -0300

    creé un DTO eliminarpractica y arreglé la vista crear practica

---
+++ TPO/src/ioo/dto/EliminarPracticaDTO.java
@@ -0,0 +1,17 @@
+package ioo.dto;
+public class EliminarPracticaDTO {
+
+    int codigoPractica;
+
+    public EliminarPracticaDTO(int codigoPractica) {
+        this.codigoPractica = codigoPractica;
+    }
+
+    public int getCodigoPractica() {
+        return codigoPractica;
+    }
+
+    public void setCodigoPractica(int codigoPractica) {
+        this.codigoPractica = codigoPractica;
+    }
+}
