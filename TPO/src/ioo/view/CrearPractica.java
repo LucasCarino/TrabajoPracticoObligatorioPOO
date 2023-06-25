0b3ea85054ee4d662686b42ba07f0cc9706b5f8c
mafebohorquezh <92961114+mafebohorquezh@users.noreply.github.com>
2023-06-24 22:24:47 -0300

    subí los cambios de lore y lo poco que hice en prácticas

--- TPO/src/ioo/view/CrearPractica.java
+++ TPO/src/ioo/view/CrearPractica.java
@@ -1,4 +1,171 @@
 package ioo.view;
 
-public class CrearPractica {
+import ioo.controller.Controller;
+import ioo.dto.PracticaDTO;
+import ioo.dto.ResultadoDTO;
+import ioo.model.Resultado;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CrearPractica extends JFrame {
+
+    private JPanel contentPane;
+    private JTextField codigoPractica;
+    private JTextField nombre;
+    private JTextField grupo;
+    private JTextField valoresCriticos;
+
+    private JTextField valoresReservados;
+    private JTextField horaParaResultado;
+    private JTextField resultado;
+
+    public CrearPractica() {
+        setResizable(false);
+        setTitle("Crear Práctica");
+
+        setBounds(100, 100, 442, 327);
+        contentPane = new JPanel();
+        contentPane.setBackground(UIManager.getColor("Table.selectionBackground"));
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+        setLocationRelativeTo(null);
+
+        JLabel lbPracticaId = new JLabel("ID Práctica:");
+        lbPracticaId.setBounds(10, 7, 120, 14);
+        contentPane.add(lbPracticaId);
+
+        codigoPractica = new JTextField();
+        codigoPractica.setBounds(124, 5, 50, 20);
+        contentPane.add(codigoPractica);
+        codigoPractica.setColumns(10);
+
+        JLabel lbnombrePractica = new JLabel("Nombre:");
+        lbnombrePractica.setBounds(10, 25, 120, 27);
+        contentPane.add(lbnombrePractica);
+
+        nombre = new JTextField();
+        nombre.setBounds(124, 28, 275, 20);
+        contentPane.add(nombre);
+        nombre.setColumns(10);
+
+        JLabel lbgrupo = new JLabel("Grupo:");
+        lbgrupo.setBounds(10, 55, 46, 14);
+        contentPane.add(lbgrupo);
+
+        grupo = new JTextField();
+        grupo.setBounds(124, 53, 156, 20);
+        contentPane.add(grupo);
+        grupo.setColumns(10);
+
+        JLabel lbvaloresCriticos = new JLabel("Valores Críticos");
+        lbvaloresCriticos.setBounds(10, 78, 120, 28);
+
+        contentPane.add(lbvaloresCriticos);
+
+        valoresCriticos = new JTextField();
+        valoresCriticos.setBounds(124, 80, 30, 20);
+
+        contentPane.add(valoresCriticos);
+        valoresCriticos.setColumns(10);
+
+        JLabel lbvaloresReservados = new JLabel("Valores Reservados:");
+        lbvaloresReservados.setBounds(10, 106, 90, 14);
+        contentPane.add(lbvaloresReservados);
+
+        valoresReservados = new JTextField();
+        valoresReservados.setBounds(124, 104, 30, 20);
+        contentPane.add(valoresReservados);
+        valoresReservados.setColumns(10);
+
+
+        JLabel lbhoraParaResultado = new JLabel("hora Para Resultado:");
+        lbhoraParaResultado.setBounds(10, 130, 100, 14);
+        contentPane.add(lbhoraParaResultado);
+
+        horaParaResultado = new JTextField();
+        horaParaResultado.setBounds(124, 128, 132, 20);
+        contentPane.add(horaParaResultado);
+        horaParaResultado.setColumns(10);
+
+
+        JLabel lbresultado = new JLabel("Resultado:");
+        lbresultado.setBounds(10, 155, 90, 14);
+        contentPane.add(lbresultado);
+
+        resultado = new JTextField();
+        resultado.setBounds(124, 154, 132, 20);
+        contentPane.add(resultado);
+        resultado.setColumns(10);
+
+        JButton btnCrearPractica = new JButton("Crear Practica");
+        btnCrearPractica.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    if (codigoPractica.getText().equalsIgnoreCase("")) {
+                        JOptionPane.showMessageDialog(null, "el campo del código de la práctica debe estar completo", "Formulario incompleto", JOptionPane.WARNING_MESSAGE);
+                    } else if (nombre.getText().equalsIgnoreCase("")) {
+                        JOptionPane.showMessageDialog(null, "el campo de nombre debe estar completo", "Formulario incompleto", JOptionPane.WARNING_MESSAGE);
+                    } else if (grupo.getText().equalsIgnoreCase("")) {
+                        JOptionPane.showMessageDialog(null, "el campo de grupo debe estar completo", "Formulario incompleto", JOptionPane.WARNING_MESSAGE);
+                    } else if (valoresCriticos.getText().equalsIgnoreCase("")) {
+                        JOptionPane.showMessageDialog(null, "el campo de valores críticos debe estar completo", "Formulario incompleto", JOptionPane.WARNING_MESSAGE);
+                    } else if (valoresReservados.getText().equalsIgnoreCase("")) {
+                        JOptionPane.showMessageDialog(null, "el campo de valores reservados debe estar completo", "Formulario incompleto", JOptionPane.WARNING_MESSAGE);
+                    } else if (horaParaResultado.getText().equalsIgnoreCase("")) {
+                        JOptionPane.showMessageDialog(null, "el campo de horaParaResultado debe estar completo", "Formulario incompleto", JOptionPane.WARNING_MESSAGE);
+                    }else if (resultado.getText().equalsIgnoreCase("")) {
+                        JOptionPane.showMessageDialog(null, "el campo de resultado debe estar completo", "Formulario incompleto", JOptionPane.WARNING_MESSAGE);
+                    }
+                    else {
+                        int codigo_practica = Integer.parseInt(codigoPractica.getText());
+                        String nombre_practica = nombre.getText();
+                        String grupo_practica = grupo.getText();
+                        int valores_criticos = Integer.parseInt(valoresCriticos.getText());
+                        boolean valores_reservados = Boolean.parseBoolean(valoresReservados.getText());
+                        int hora_Para_Resultado = Integer.parseInt(horaParaResultado.getText());
+                        String resultado_text = resultado.getText();
+
+                        Resultado resultado_practica = new Resultado(resultado_text, hora_Para_Resultado,  )
+
+                        PracticaDTO nueva_practica = new PracticaDTO(codigo_practica, nombre_practica, grupo_practica, valores_criticos,
+                                valores_reservados, hora_Para_Resultado, resultado_practica);
+                        boolean respuesta = Controller.getControlador().crearPaciente(nuevo_paciente);
+                        Controller.getPacientes();
+
+                        if (respuesta) {
+                            JOptionPane.showMessageDialog(null, "El paciente se ha creado correctamente", "Paciente Creado", JOptionPane.INFORMATION_MESSAGE);
+                        } else {
+                            JOptionPane.showMessageDialog(null, "El paciente ya existe en el sistema", "Paciente Duplicado", JOptionPane.ERROR_MESSAGE);
+                        }
+                        nroPaciente.setText("");
+                        NombrePaciente.setText("");
+                        DNIPaciente.setText("");
+                        EdadPaciente.setText("");
+                        SexoPaciente.setText("");
+                        DomicilioPaciente.setText("");
+                        MailPaciente.setText("");
+                    }
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null, "no ingrese caracteres en los campos de solo numeros", "Error caracter ingresado erroneamente", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+        btnCrearPaciente.setBounds(170, 266, 140, 20);
+        contentPane.add(btnCrearPaciente);
+
+        JButton btnNewButton = new JButton("Volver atr\u00E1s");
+        btnNewButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+        btnNewButton.setBounds(0, 266, 130, 20);
+        contentPane.add(btnNewButton);
+    }
 }
+
+
