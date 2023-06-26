3233ee51b90e9203d8b90bff44f9fee9746ba8f3
mafebohorquezh <92961114+mafebohorquezh@users.noreply.github.com>
2023-06-25 22:20:07 -0300

    eliminé cliente y domicilio que era viejo

--- TPO/src/ioo/model/Cliente.java
+++
@@ -1,9 +0,0 @@
-package ioo.model;
-import java.util.Date;
-public class Cliente {
-    private String sexo;
-    private int edad;
-    private int dni;
-    private String fechaNacimiento;
-    private Domicilio domicilio;
-}
